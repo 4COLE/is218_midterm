114133bc049b64b4e560c2348a16ef78179ddbe0
4COLE <cole@cole-laptop>
2024-11-08 15:47:41 -0500

    updated readme.md

--- plugins/sqrt.py
+++ plugins/sqrt.py
@@ -1,6 +1,6 @@
 """Example plugin to calculate square root of a number."""
 import math
-def sqrt(a):
+def sqrt(a, *args):
     """Calculate square root of a number."""
     return math.sqrt(a)
 plugin = {
