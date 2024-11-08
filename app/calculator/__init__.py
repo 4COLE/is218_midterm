a78e08c16c849b60ab49647c43ce64ca8ae5a182
4COLE <cole@cole-laptop>
2024-11-08 15:28:48 -0500

    added modulo and power to basic calculations

--- app/calculator/__init__.py
+++ app/calculator/__init__.py
@@ -9,16 +9,19 @@ class Calculator:
     def __init__(self):
         self.calculation = BasicCalculation()
         self.history_manager = historymanager()
+        """ Add modulo and power to operations"""
         self.operations = {
             "add": self.calculation.calculate,
             "subtract": self.calculation.calculate,
             "multiply": self.calculation.calculate,
-            "divide": self.calculation.calculate
+            "divide": self.calculation.calculate,
+            "modulo": self.calculation.calculate,
+            "power": self.calculation.calculate
         }
         self.load_plugins()
 
     def load_plugins(self):
-        """Dynamically load plugins from the plugins dir."""
+        """Dynamically load plugins from the plugins directory."""
         plugins_dir = "plugins"
         for filename in os.listdir(plugins_dir):
             if filename.endswith(".py"):
@@ -31,8 +34,8 @@ class Calculator:
         """Calculate the result, log the operation in history, and return the result or error."""
         func = self.operations.get(operation)
         if func:
-            """Use the function based on the operation, handling single and double operand cases"""
-            result = func(a, b, operation) if operation in ["add", "subtract", "multiply", "divide"] else func(a)
+            """ Use the function for binary operations"""
+            result = func(a, b, operation)
             if isinstance(result, (int, float)):
                 entry = f"{a} {operation} {b} = {result}"
                 self.history_manager.add_to_history(entry)
